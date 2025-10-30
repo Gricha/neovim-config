69dbd650dd989300138695a543b23200d397e8d0
Greg Pstrucha <875316+Gricha@users.noreply.github.com>
2025-10-30 21:45:28 +0000

    fix nvim config

--- lua/plugins/force-ts-lsp.lua
+++ lua/plugins/force-ts-lsp.lua
@@ -1,10 +1,48 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      denols = {
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local util = require("lspconfig.util")
+      opts.servers = opts.servers or {}
+
+      opts.servers.denols = {
         enabled = false,
-      },
-    },
+        root_dir = util.root_pattern("deno.json", "deno.jsonc"),
+      }
+
+      opts.servers.vtsls = vim.tbl_deep_extend("force", opts.servers.vtsls or {}, {
+        root_dir = function(bufnr, on_dir)
+          local fname = vim.api.nvim_buf_get_name(bufnr)
+          local found = util.root_pattern("tsconfig.json", "package.json")(fname) or util.find_git_ancestor(fname)
+          if found then
+            on_dir(found)
+          end
+        end,
+        single_file_support = true,
+      })
+
+      opts.servers.eslint = vim.tbl_deep_extend("force", opts.servers.eslint or {}, {
+        root_dir = function(bufnr, on_dir)
+          local fname = vim.api.nvim_buf_get_name(bufnr)
+          local found = util.root_pattern(
+            "eslint.config.js",
+            "eslint.config.mjs",
+            "eslint.config.cjs",
+            ".eslintrc",
+            ".eslintrc.js",
+            ".eslintrc.cjs",
+            ".eslintrc.json",
+            "package.json"
+          )(fname) or util.find_git_ancestor(fname)
+          if found then
+            on_dir(found)
+          end
+        end,
+        settings = {
+          workingDirectories = { mode = "auto" },
+          experimental = { useFlatConfig = true },
+        },
+      })
+    end,
   },
 }
